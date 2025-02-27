27af026eda937885ae0baeea9532fdc9caccde14
Michael Angelo B. Zara <michaelangelobuccatzara@gmail.com>
2025-02-28 01:24:50 +0800

    Register stomp endpoint added a custom interceptor

--- src/main/java/com/DTEC/Document_Tracking_and_E_Clearance/web_socket/WebSocketConfig.java
+++ src/main/java/com/DTEC/Document_Tracking_and_E_Clearance/web_socket/WebSocketConfig.java
@@ -1,5 +1,7 @@
 package com.DTEC.Document_Tracking_and_E_Clearance.web_socket;
 
+import com.DTEC.Document_Tracking_and_E_Clearance.configuration.JwtHandshakeInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -7,6 +9,7 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@Slf4j
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
@@ -14,11 +17,19 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Value("${frontend.origin}")
     private String FRONTEND_ORIGIN;
 
+    private final JwtHandshakeInterceptor jwtHandshakeInterceptor;
+
+    public WebSocketConfig(JwtHandshakeInterceptor jwtHandshakeInterceptor) {
+        this.jwtHandshakeInterceptor = jwtHandshakeInterceptor;
+    }
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        log.info("Register Stomp Endpoints trigger");
         registry
                 .addEndpoint("/wss")
                 .setAllowedOrigins(FRONTEND_ORIGIN)
+                .addInterceptors(jwtHandshakeInterceptor)
                 .withSockJS();
     }
 
@@ -26,6 +37,5 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
         registry.enableSimpleBroker("/topic");
-        registry.setUserDestinationPrefix("/user");
     }
 }
